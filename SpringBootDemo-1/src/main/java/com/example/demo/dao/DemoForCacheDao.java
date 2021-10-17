0b93c353aeeea5555ed883a08a7764d4a0d2493c
lxyspotless <lxyspotless@163.com>
2021-10-17 23:48:47 +0800

    1017代码更新

---
+++ SpringBootDemo-1/src/main/java/com/example/demo/dao/DemoForCacheDao.java
@@ -0,0 +1,35 @@
+package com.example.demo.dao;
+
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.City;
+
+@Repository
+public class DemoForCacheDao {
+	
+	static Map<Integer,City> dataMap = Collections.synchronizedMap(new HashMap<Integer,City>());
+
+	public void addCity(City city) throws Exception{
+
+		if (null != city ) {
+			City data = dataMap.get(city.getId());
+			if (null == data) {
+				dataMap.put(city.getId(), city);
+			}
+			else {
+				throw new Exception("数据已存在");
+			}
+		}
+	}
+	
+	public Map<Integer,City> findAllCity(){
+		return dataMap;
+	}
+
+	
+}
