dd3f00743a0be990fb04cd1e355e31db3e76ffee
lxyspotless <lxyspotless@163.com>
2021-10-17 23:46:25 +0800

    1017代码更新

--- SpringBootDemo-1/src/main/java/com/example/demo/controller/DemoForCacheController.java
+++ SpringBootDemo-1/src/main/java/com/example/demo/controller/DemoForCacheController.java
@@ -1,15 +1,37 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.demo.entity.City;
+import com.example.demo.service.DemoForCacheService;
 
 @Controller
 public class DemoForCacheController {
-	@RequestMapping
-	public String addCity() {
+	
+	@Autowired
+	DemoForCacheService demoForCaheService;
+	
+	@RequestMapping("/addCity")
+	public String addCity(@ModelAttribute City city,ModelMap map) {
+		System.out.println(ToStringBuilder.reflectionToString(city));
+		String msgString = demoForCaheService.addCity(city);
+		map.put("returnMsg", msgString);
+		return "addCity";
+	}
+	
+	
+	@RequestMapping("/findAllCity")
+	public String findAllCity(ModelMap map) {
 		
-		
-		return "list";
+		List<City> cityList = demoForCaheService.findAllCity();
+		map.put("cityList", cityList);
+		return "findAllCity";
 	}
 
 }
